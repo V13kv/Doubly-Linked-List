af482d0c8dfac5a76c21badab1bce72d4f41f2d7
Mate <skvortsov.vi@phystech.edu>
2022-07-17 16:24:50 +0300

    Some main.cpp changes

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,28 @@ int main()
 {
     srand((unsigned int) time(NULL));
 
-
     list_t list = {};
     IS_ERROR(listCtor(&list, 4))
     {
         EXIT_WITH_TRACE(EXIT_CODES::CONSTRUCTOR_ERROR, EXIT_FAILURE);
     }
 
-    for (int i = 0; i < 2; ++i)
+    for (int i = 0; i < 4; ++i)
     {
         listInsertAfter(&list, i, i + 1);
     }
 
     listRemove(&list, 2);
-    listInsertAfter(&list, 1, 555);
+    listInsertAfter(&list, 4, 555);
     listAppend(&list, 888);
     listAppend(&list, 777);
-    listAppend(&list, 999);
-    listAppend(&list, 54321);
-    listCreateGraph(&list);
+
+    int idx = 0;
+    listGetNextNode(&list, 4, &idx);
+    printf("%d\n", idx);
+
+    listGetPrevNode(&list, 5, &idx);
+    printf("%d\n", idx); 
 
     IS_ERROR(listDtor(&list))
     {
